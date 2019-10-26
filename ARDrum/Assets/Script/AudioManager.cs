3003c2bf13ed919e7c64a9adde3e3f12b64d6963
kugi <kugimania@gmail.com>
2019-10-27 03:21:27 +0900

    Add Audio Manager

---
+++ ARDrum/Assets/Script/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip Bass;
+    public AudioClip Cymbal;
+    public AudioClip Hat;
+	public AudioClip Snare;
+	public AudioSource audioSource;
+
+
+    void Start()
+    {
+		audioSource = GetComponent<AudioSource>();
+    }
+
+    public void playBass()
+	{
+        audioSource.PlayOneShot(Bass);
+	}
+
+	public void playCymbal()
+	{
+        audioSource.PlayOneShot(Cymbal);
+	}
+
+    public void playHat()
+	{
+        audioSource.PlayOneShot(Hat);
+	}
+
+    public void playSnare()
+	{
+        audioSource.PlayOneShot(Snare);
+	}
+}
---
+++ ARDrum/Assets/Script/CustomDefaultTrackableEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class CustomDefaultTrackableEventHandler : DefaultTrackableEventHandler
+{
+
+	public UnityEvent OnTrackingAction;
+	public UnityEvent OffTrackingAction;
+
+	protected override void OnTrackingFound()
+	{
+		base.OnTrackingFound();
+		OnTrackingAction.Invoke();
+	}
+
+	protected override void OnTrackingLost()
+	{
+		base.OnTrackingLost();
+		OffTrackingAction.Invoke();
+	}
+}
