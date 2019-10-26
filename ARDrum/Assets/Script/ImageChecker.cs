f8eb972f00c1252b2d1b1e632ab663c625e24ba9
kugi <kugimania@gmail.com>
2019-10-26 16:15:44 +0900

    Add Script

---
+++ ARDrum/Assets/Script/ImageChecker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageChecker : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    bool changed;
+    bool isFirst;
+    private void Start()
+    {
+        isFirst = true;
+        changed = false;
+        meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CheckImage();
+    }
+
+    void CheckImage()
+    {
+        if (meshRenderer.enabled == false && changed != meshRenderer.enabled)
+        {
+            Debug.Log("Disappeared!!");
+        }
+        changed = meshRenderer.enabled;
+    }
+}
