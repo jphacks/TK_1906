8a697876c29b06a367e39e456addd235a4ee877a
tyanio0627 <tyanio_0627@yahoo.co.jp>
2019-10-27 12:31:30 +0900

    modify choose sound

---
+++ ARDrum/Assets/Script/ChooseSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseSound : MonoBehaviour
+{
+    string soundString = "Bass";
+
+    public void ChoseSound()
+    {
+        switch (GetComponent<Dropdown>().value)
+        {
+            case 0:
+                soundString = "Bass";
+                break;
+            case 1:
+                soundString = "Cymbal";
+                break;
+            case 2:
+                soundString = "Hat";
+                break;
+            case 3:
+                soundString = "Snare";
+                break;
+            default:
+                soundString = "NULL";
+                break;
+        }
+    }
+
+    public string GetCurrentSound()
+    {
+        return soundString;
+    }
+
+}
